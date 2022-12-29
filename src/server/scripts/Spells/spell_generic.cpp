80f57f5d00158ac10513de03f73d120cb68dc25c
leelf00 <leelf_00@126.com>
2022-12-29 19:32:48 +0800

    Update spell_generic.cpp

--- src/server/scripts/Spells/spell_generic.cpp
+++ src/server/scripts/Spells/spell_generic.cpp
@@ -4728,7 +4728,7 @@ class spell_gen_rocket_barrage : public SpellScript
             // Get highest attack power
             uint32 ap = std::max(caster->GetTotalAttackPowerValue(BASE_ATTACK), caster->GetTotalAttackPowerValue(RANGED_ATTACK));
 
-            SetHitDamage(1 + (0.25f * ap) + (0.429f * caster->SpellBaseDamageBonusDone(SPELL_SCHOOL_MASK_FIRE)) + (caster->getLevel() * 2));
+            SetHitDamage(1 + (0.25f * ap) + (0.429f * caster->SpellBaseDamageBonusDone(SPELL_SCHOOL_MASK_FIRE)) + (caster->GetLevel() * 2));
         }
     }
 
