57b924a41d62b295c07cb67d67305c7a49e8e346
leelf00 <leelf_00@126.com>
2023-04-09 16:03:18 +0800

    [Cmake/GCC]Fix arm64 gcc compile error

--- cmake/compiler/gcc/settings.cmake
+++ cmake/compiler/gcc/settings.cmake
@@ -14,16 +14,6 @@ add_definitions(-D_BUILD_DIRECTIVE='"${CMAKE_BUILD_TYPE}"')
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections -s -w" CACHE INTERNAL "EXE_LINKER_FLAGS" FORCE)
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -ffunction-sections -fdata-sections" CACHE INTERNAL "C_FLAGS" FORCE)
 
-
-if(PLATFORM EQUAL 32)
-  # Required on 32-bit systems to enable SSE2 (standard on x64)
-  set(SSE_FLAGS "-msse2 -mfpmath=sse")
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${SSE_FLAGS}")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${SSE_FLAGS}")
-endif()
-add_definitions(-DHAVE_SSE2 -D__SSE2__)
-message(STATUS "GCC: SFMT enabled, SSE2 flags forced")
-
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
 
 set(COMPILER_FLAGS "")
