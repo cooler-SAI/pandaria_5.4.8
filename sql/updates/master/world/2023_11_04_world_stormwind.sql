543532f2d8d6a068c2794b66b50f6c7567252022
leelf00 <leelf_00@126.com>
2023-11-05 00:28:38 +0800

    [Scripts/Stormwind] Move some db_script_string to creature text

---
+++ sql/updates/master/world/2023_11_04_world_stormwind.sql
@@ -0,0 +1,241 @@
+-- Convert some db_script_string to creature text
+UPDATE `waypoint_scripts` SET `dataint` = 5960 WHERE `dataint` = 2000005018;
+UPDATE `waypoint_scripts` SET `dataint` = 22392 WHERE `dataint` = 2000005019;
+UPDATE `waypoint_scripts` SET `dataint` = 22050 WHERE `dataint` = 2000005020;
+UPDATE `waypoint_scripts` SET `dataint` = 22051 WHERE `dataint` = 2000005021;
+UPDATE `waypoint_scripts` SET `dataint` = 22052 WHERE `dataint` = 2000005022;
+UPDATE `waypoint_scripts` SET `dataint` = 10141 WHERE `dataint` = 2000005023;
+UPDATE `waypoint_scripts` SET `dataint` = 10146 WHERE `dataint` = 2000005024;
+UPDATE `waypoint_scripts` SET `dataint` = 10152 WHERE `dataint` = 2000005025;
+UPDATE `waypoint_scripts` SET `dataint` = 10126 WHERE `dataint` = 2000005026;
+UPDATE `waypoint_scripts` SET `dataint` = 7194 WHERE `dataint` = 2000005027;
+UPDATE `waypoint_scripts` SET `dataint` = 7193 WHERE `dataint` = 2000005028;
+UPDATE `waypoint_scripts` SET `dataint` = 7191 WHERE `dataint` = 2000005029;
+UPDATE `waypoint_scripts` SET `dataint` = 10139 WHERE `dataint` = 2000005030; -- 4.x text
+UPDATE `waypoint_scripts` SET `dataint` = 6406 WHERE `dataint` = 2000005031;
+UPDATE `waypoint_scripts` SET `dataint` = 6405 WHERE `dataint` = 2000005032;
+UPDATE `waypoint_scripts` SET `dataint` = 10158 WHERE `dataint` = 2000005033;
+UPDATE `waypoint_scripts` SET `dataint` = 22544 WHERE `dataint` = 2000005034;
+UPDATE `waypoint_scripts` SET `dataint` = 10146 WHERE `dataint` = 2000005035;
+UPDATE `waypoint_scripts` SET `dataint` = 12835 WHERE `dataint` = 2000005036;
+UPDATE `waypoint_scripts` SET `dataint` = 12832 WHERE `dataint` = 2000005037;
+UPDATE `waypoint_scripts` SET `dataint` = 12833 WHERE `dataint` = 2000005038;
+UPDATE `waypoint_scripts` SET `dataint` = 14565 WHERE `dataint` = 2000005039;
+UPDATE `waypoint_scripts` SET `dataint` = 14567 WHERE `dataint` = 2000005040;
+UPDATE `waypoint_scripts` SET `dataint` = 15144 WHERE `dataint` = 2000005041;
+UPDATE `waypoint_scripts` SET `dataint` = 15373 WHERE `dataint` = 2000005042;
+UPDATE `waypoint_scripts` SET `dataint` = 17569 WHERE `dataint` = 2000005043; -- doesn't exist anywhere, picked a random text from the same npc
+UPDATE `waypoint_scripts` SET `dataint` = 17563 WHERE `dataint` = 2000005044; -- doesn't exist anywhere, picked a random text from the same npc
+UPDATE `waypoint_scripts` SET `dataint` = 16059 WHERE `dataint` = 2000005045;
+UPDATE `waypoint_scripts` SET `dataint` = 10119 WHERE `dataint` = 2000005048;
+UPDATE `waypoint_scripts` SET `dataint` = 17035 WHERE `dataint` = 2000005049;
+UPDATE `waypoint_scripts` SET `dataint` = 17045 WHERE `dataint` = 2000005050;
+UPDATE `waypoint_scripts` SET `dataint` = 17056 WHERE `dataint` = 2000005051;
+UPDATE `waypoint_scripts` SET `dataint` = 17486 WHERE `dataint` = 2000005052;
+UPDATE `waypoint_scripts` SET `dataint` = 17485 WHERE `dataint` = 2000005053;
+UPDATE `waypoint_scripts` SET `dataint` = 17485 WHERE `dataint` = 2000005054;
+UPDATE `waypoint_scripts` SET `dataint` = 17485 WHERE `dataint` = 2000005055;
+UPDATE `waypoint_scripts` SET `dataint` = 18816 WHERE `dataint` = 2000005056;
+UPDATE `waypoint_scripts` SET `dataint` = 709 WHERE `dataint` = 2000005057;
+UPDATE `waypoint_scripts` SET `dataint` = 710 WHERE `dataint` = 2000005058;
+UPDATE `waypoint_scripts` SET `dataint` = 705 WHERE `dataint` = 2000005059;
+UPDATE `waypoint_scripts` SET `dataint` = 693 WHERE `dataint` = 2000005060;
+UPDATE `waypoint_scripts` SET `dataint` = 710 WHERE `dataint` = 2000005061;
+UPDATE `waypoint_scripts` SET `dataint` = 706 WHERE `dataint` = 2000005062;
+UPDATE `waypoint_scripts` SET `dataint` = 709 WHERE `dataint` = 2000005063;
+UPDATE `waypoint_scripts` SET `dataint` = 710 WHERE `dataint` = 2000005064;
+UPDATE `waypoint_scripts` SET `dataint` = 705 WHERE `dataint` = 2000005065;
+UPDATE `waypoint_scripts` SET `dataint` = 693 WHERE `dataint` = 2000005066;
+UPDATE `waypoint_scripts` SET `dataint` = 710 WHERE `dataint` = 2000005067;
+UPDATE `waypoint_scripts` SET `dataint` = 706 WHERE `dataint` = 2000005068;
+UPDATE `waypoint_scripts` SET `dataint` = 709 WHERE `dataint` = 2000005069;
+UPDATE `waypoint_scripts` SET `dataint` = 710 WHERE `dataint` = 2000005070;
+UPDATE `waypoint_scripts` SET `dataint` = 705 WHERE `dataint` = 2000005071;
+UPDATE `waypoint_scripts` SET `dataint` = 693 WHERE `dataint` = 2000005072;
+UPDATE `waypoint_scripts` SET `dataint` = 710 WHERE `dataint` = 2000005073;
+UPDATE `waypoint_scripts` SET `dataint` = 706 WHERE `dataint` = 2000005074;
+UPDATE `waypoint_scripts` SET `dataint` = 709 WHERE `dataint` = 2000005075;
+UPDATE `waypoint_scripts` SET `dataint` = 710 WHERE `dataint` = 2000005076;
+UPDATE `waypoint_scripts` SET `dataint` = 705 WHERE `dataint` = 2000005077;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005078;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005079;
+UPDATE `waypoint_scripts` SET `dataint` = 696 WHERE `dataint` = 2000005080;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005081;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005082;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005083;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005084;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005085;
+UPDATE `waypoint_scripts` SET `dataint` = 696 WHERE `dataint` = 2000005086;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005087;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005088;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005089;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005090;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005091;
+UPDATE `waypoint_scripts` SET `dataint` = 696 WHERE `dataint` = 2000005092;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005093;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005094;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005095;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005096;
+UPDATE `waypoint_scripts` SET `dataint` = 694 WHERE `dataint` = 2000005097;
+UPDATE `waypoint_scripts` SET `dataint` = 696 WHERE `dataint` = 2000005098;
+UPDATE `waypoint_scripts` SET `dataint` = 1101 WHERE `dataint` = 2000005099;
+UPDATE `waypoint_scripts` SET `dataint` = 1103 WHERE `dataint` = 2000005100;
+UPDATE `waypoint_scripts` SET `dataint` = 4013 WHERE `dataint` = 2000005101;
+UPDATE `waypoint_scripts` SET `dataint` = 1104 WHERE `dataint` = 2000005102;
+UPDATE `waypoint_scripts` SET `dataint` = 1110 WHERE `dataint` = 2000005103;
+UPDATE `waypoint_scripts` SET `dataint` = 1105 WHERE `dataint` = 2000005104;
+UPDATE `waypoint_scripts` SET `dataint` = 1108 WHERE `dataint` = 2000005105;
+UPDATE `waypoint_scripts` SET `dataint` = 1106 WHERE `dataint` = 2000005106;
+UPDATE `waypoint_scripts` SET `dataint` = 1107 WHERE `dataint` = 2000005107;
+UPDATE `waypoint_scripts` SET `dataint` = 1112 WHERE `dataint` = 2000005108;
+UPDATE `waypoint_scripts` SET `dataint` = 151 WHERE `dataint` = 2000005109;
+UPDATE `waypoint_scripts` SET `dataint` = 151 WHERE `dataint` = 2000005110;
+UPDATE `waypoint_scripts` SET `dataint` = 155 WHERE `dataint` = 2000005111;
+UPDATE `waypoint_scripts` SET `dataint` = 154 WHERE `dataint` = 2000005112;
+UPDATE `waypoint_scripts` SET `dataint` = 152 WHERE `dataint` = 2000005113;
+UPDATE `waypoint_scripts` SET `dataint` = 177 WHERE `dataint` = 2000005114;
+UPDATE `waypoint_scripts` SET `dataint` = 149 WHERE `dataint` = 2000005115;
+UPDATE `waypoint_scripts` SET `dataint` = 173 WHERE `dataint` = 2000005116;
+UPDATE `waypoint_scripts` SET `dataint` = 174 WHERE `dataint` = 2000005117;
+UPDATE `waypoint_scripts` SET `dataint` = 172 WHERE `dataint` = 2000005118;
+UPDATE `waypoint_scripts` SET `dataint` = 173 WHERE `dataint` = 2000005119;
+UPDATE `waypoint_scripts` SET `dataint` = 172 WHERE `dataint` = 2000005120;
+UPDATE `waypoint_scripts` SET `dataint` = 176 WHERE `dataint` = 2000005121;
+UPDATE `waypoint_scripts` SET `dataint` = 178 WHERE `dataint` = 2000005122;
+UPDATE `waypoint_scripts` SET `dataint` = 169 WHERE `dataint` = 2000005123;
+UPDATE `waypoint_scripts` SET `dataint` = 147 WHERE `dataint` = 2000005124;
+UPDATE `waypoint_scripts` SET `dataint` = 157 WHERE `dataint` = 2000005125;
+UPDATE `waypoint_scripts` SET `dataint` = 168 WHERE `dataint` = 2000005126;
+UPDATE `waypoint_scripts` SET `dataint` = 147 WHERE `dataint` = 2000005127;
+UPDATE `waypoint_scripts` SET `dataint` = 157 WHERE `dataint` = 2000005128;
+UPDATE `waypoint_scripts` SET `dataint` = 167 WHERE `dataint` = 2000005129;
+UPDATE `waypoint_scripts` SET `dataint` = 157 WHERE `dataint` = 2000005130;
+UPDATE `waypoint_scripts` SET `dataint` = 147 WHERE `dataint` = 2000005131;
+UPDATE `waypoint_scripts` SET `dataint` = 2874 WHERE `dataint` = 2000005132;
+UPDATE `waypoint_scripts` SET `dataint` = 10119 WHERE `dataint` = 2000005133;
+UPDATE `waypoint_scripts` SET `dataint` = 1129 WHERE `dataint` = 2000005134;
+UPDATE `waypoint_scripts` SET `dataint` = 1154 WHERE `dataint` = 2000005135;
+UPDATE `waypoint_scripts` SET `dataint` = 1155 WHERE `dataint` = 2000005136;
+UPDATE `waypoint_scripts` SET `dataint` = 1140 WHERE `dataint` = 2000005137;
+UPDATE `waypoint_scripts` SET `dataint` = 1161 WHERE `dataint` = 2000005138;
+UPDATE `waypoint_scripts` SET `dataint` = 1162 WHERE `dataint` = 2000005139;
+UPDATE `waypoint_scripts` SET `dataint` = 1163 WHERE `dataint` = 2000005140;
+UPDATE `waypoint_scripts` SET `dataint` = 1165 WHERE `dataint` = 2000005141;
+UPDATE `waypoint_scripts` SET `dataint` = 1166 WHERE `dataint` = 2000005142;
+UPDATE `waypoint_scripts` SET `dataint` = 13580 WHERE `dataint` = 2000005144;
+UPDATE `waypoint_scripts` SET `dataint` = 13576 WHERE `dataint` = 2000005145;
+UPDATE `waypoint_scripts` SET `dataint` = 15119 WHERE `dataint` = 2000005147;
+UPDATE `waypoint_scripts` SET `dataint` = 209 WHERE `dataint` = 2000005148;
+UPDATE `waypoint_scripts` SET `dataint` = 224 WHERE `dataint` = 2000005149;
+UPDATE `waypoint_scripts` SET `dataint` = 225 WHERE `dataint` = 2000005150;
+UPDATE `waypoint_scripts` SET `dataint` = 213 WHERE `dataint` = 2000005151;
+UPDATE `waypoint_scripts` SET `dataint` = 225 WHERE `dataint` = 2000005152;
+UPDATE `waypoint_scripts` SET `dataint` = 225 WHERE `dataint` = 2000005153;
+UPDATE `waypoint_scripts` SET `dataint` = 224 WHERE `dataint` = 2000005154;
+UPDATE `waypoint_scripts` SET `dataint` = 214 WHERE `dataint` = 2000005155;
+UPDATE `waypoint_scripts` SET `dataint` = 217 WHERE `dataint` = 2000005156;
+UPDATE `waypoint_scripts` SET `dataint` = 210 WHERE `dataint` = 2000005157;
+UPDATE `waypoint_scripts` SET `dataint` = 205 WHERE `dataint` = 2000005158;
+UPDATE `waypoint_scripts` SET `dataint` = 216 WHERE `dataint` = 2000005159;
+UPDATE `waypoint_scripts` SET `dataint` = 212 WHERE `dataint` = 2000005160;
+UPDATE `waypoint_scripts` SET `dataint` = 208 WHERE `dataint` = 2000005161;
+UPDATE `waypoint_scripts` SET `dataint` = 214 WHERE `dataint` = 2000005162;
+UPDATE `waypoint_scripts` SET `dataint` = 215 WHERE `dataint` = 2000005163;
+UPDATE `waypoint_scripts` SET `dataint` = 206 WHERE `dataint` = 2000005164;
+UPDATE `waypoint_scripts` SET `dataint` = 212 WHERE `dataint` = 2000005165;
+UPDATE `waypoint_scripts` SET `dataint` = 214 WHERE `dataint` = 2000005166;
+UPDATE `waypoint_scripts` SET `dataint` = 1153 WHERE `dataint` = 2000005167;
+UPDATE `waypoint_scripts` SET `dataint` = 1130 WHERE `dataint` = 2000005168;
+UPDATE `waypoint_scripts` SET `dataint` = 201 WHERE `dataint` = 2000005169;
+UPDATE `waypoint_scripts` SET `dataint` = 218 WHERE `dataint` = 2000005170;
+UPDATE `waypoint_scripts` SET `dataint` = 222 WHERE `dataint` = 2000005171;
+UPDATE `waypoint_scripts` SET `dataint` = 221 WHERE `dataint` = 2000005172;
+UPDATE `waypoint_scripts` SET `dataint` = 204 WHERE `dataint` = 2000005173;
+UPDATE `waypoint_scripts` SET `dataint` = 222 WHERE `dataint` = 2000005174;
+UPDATE `waypoint_scripts` SET `dataint` = 219 WHERE `dataint` = 2000005175;
+UPDATE `waypoint_scripts` SET `dataint` = 218 WHERE `dataint` = 2000005176;
+UPDATE `waypoint_scripts` SET `dataint` = 221 WHERE `dataint` = 2000005177;
+UPDATE `waypoint_scripts` SET `dataint` = 219 WHERE `dataint` = 2000005178;
+UPDATE `waypoint_scripts` SET `dataint` = 222 WHERE `dataint` = 2000005179;
+UPDATE `waypoint_scripts` SET `dataint` = 587 WHERE `dataint` = 2000005180;
+UPDATE `waypoint_scripts` SET `dataint` = 589 WHERE `dataint` = 2000005181;
+UPDATE `waypoint_scripts` SET `dataint` = 590 WHERE `dataint` = 2000005182;
+UPDATE `waypoint_scripts` SET `dataint` = 588 WHERE `dataint` = 2000005183;
+UPDATE `waypoint_scripts` SET `dataint` = 1091 WHERE `dataint` = 2000005184;
+UPDATE `waypoint_scripts` SET `dataint` = 24425 WHERE `dataint` = 2000005200;
+UPDATE `waypoint_scripts` SET `dataint` = 24425 WHERE `dataint` = 2000005201;
+UPDATE `waypoint_scripts` SET `dataint` = 22216 WHERE `dataint` = 2000005202;
+UPDATE `waypoint_scripts` SET `dataint` = 22211 WHERE `dataint` = 2000005203;
+UPDATE `waypoint_scripts` SET `dataint` = 22206 WHERE `dataint` = 2000005204;
+UPDATE `waypoint_scripts` SET `dataint` = 25086 WHERE `dataint` = 2000005205;
+UPDATE `waypoint_scripts` SET `dataint` = 25073 WHERE `dataint` = 2000005206;
+UPDATE `waypoint_scripts` SET `dataint` = 25074 WHERE `dataint` = 2000005207;
+UPDATE `waypoint_scripts` SET `dataint` = 25075 WHERE `dataint` = 2000005208;
+UPDATE `waypoint_scripts` SET `dataint` = 25076 WHERE `dataint` = 2000005209;
+UPDATE `waypoint_scripts` SET `dataint` = 23842 WHERE `dataint` = 2000005210;
+UPDATE `waypoint_scripts` SET `dataint` = 328 WHERE `dataint` = 2000005400;
+UPDATE `waypoint_scripts` SET `dataint` = 327 WHERE `dataint` = 2000005401;
+UPDATE `waypoint_scripts` SET `dataint` = 5956 WHERE `dataint` = 2000005402;
+UPDATE `waypoint_scripts` SET `dataint` = 5957 WHERE `dataint` = 2000005403;
+UPDATE `waypoint_scripts` SET `dataint` = 5960 WHERE `dataint` = 2000005404;
+UPDATE `waypoint_scripts` SET `dataint` = 5959 WHERE `dataint` = 2000005405;
+UPDATE `waypoint_scripts` SET `dataint` = 24439 WHERE `dataint` = 2000005406;
+UPDATE `waypoint_scripts` SET `dataint` = 24443 WHERE `dataint` = 2000005407;
+UPDATE `waypoint_scripts` SET `dataint` = 36 WHERE `dataint` = 2000005408;
+
+-- Convert Erich Lohan (3627) text to creature text
+DELETE FROM `creature_text` WHERE `CreatureID`=3627;
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES 
+(3627, 0, 0, 'Magical studies stressing your brain? Relax at the Blue Recluse!', 12, 7, 100, 0, 0, 0, 1197, 0, 'Erich Lohan'),
+(3627, 0, 1, 'Best drinks in Stormwind!', 12, 7, 100, 0, 0, 0, 1195, 0, 'Erich Lohan'),
+(3627, 0, 2, 'Come for the beer, stay for the atmosphere!', 12, 7, 100, 0, 0, 0, 1196, 0, 'Erich Lohan'),
+(3627, 0, 3, 'The only place in Stormwind where magic and spirits mix, come to the Blue Recluse!', 12, 7, 100, 0, 0, 0, 1198, 0, 'Erich Lohan'),
+(3627, 0, 4, 'Free drinks at the Blue Recluse!', 12, 7, 100, 0, 0, 0, 1193, 0, 'Erich Lohan'),
+(3627, 0, 5, 'Feeling blue? Come on down to the Blue Recluse for a good time!', 12, 7, 100, 0, 0, 0, 1199, 0, 'Erich Lohan'),
+(3627, 0, 6, 'Head on over to the Blue Recluse. Where everybody knows your name!', 12, 7, 100, 0, 0, 0, 1194, 0, 'Erich Lohan'),
+(3627, 0, 7, 'Ask me how to get a free drink at the Blue Recluse!', 12, 7, 100, 0, 0, 0, 1192, 0, 'Erich Lohan');
+
+UPDATE `waypoint_data` SET `action`=0 WHERE `id`=5831;
+DELETE FROM `db_script_string` WHERE `entry` BETWEEN 2000005185 AND 2000005199;
+DELETE FROM `waypoint_scripts` WHERE `dataint` BETWEEN 2000005185 AND 2000005199;
+
+-- Apprentice Morlann(23600)
+DELETE FROM `creature_text` WHERE `CreatureID` IN (23600,23601);
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `BroadcastTextId`, `TextRange`, `comment`) VALUES 
+(23600, 0, 0, 'Hmm... I wonder...', 12, 0, 100, 0, 0, 0, 22042, 0, 'Apprentice Morlann'),
+(23600, 1, 0, 'Now, where is it?', 12, 0, 100, 6, 0, 0, 22045, 0, 'Apprentice Morlann'),
+(23600, 2, 0, 'Stupid mages... always using the last of a reagent and never replacing it.', 12, 0, 100, 1, 0, 0, 22043, 0, 'Apprentice Morlann'),
+(23601, 0, 0, 'Is he complaining about the reagents again? Does he even remember how he left the summoning tomes last time?', 12, 0, 100, 6, 0, 0, 22044, 0, 'Apprentice Garion');
+
+UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=23600;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=23600 AND `source_type`=0;
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (2360000,2360001) AND `source_type`=9;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`event_param5`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
+(23600,0,0,0,25,0,100,0,0,0,0,0,0,53,0,23600,1,0,0,0,1,0,0,0,0,0,0,0,"Apprentice Garion - On Reset - Start Waypoint"),
+(23600,0,1,0,40,0,100,0,1,23600,0,0,0,80,2360000,0,0,0,0,0,1,0,0,0,0,0,0,0,"Apprentice Garion - On Waypoint 1 Reached - Run Script"),
+(23600,0,2,0,40,0,100,0,4,23600,0,0,0,80,2360001,0,0,0,0,0,1,0,0,0,0,0,0,0,"Apprentice Garion - On Waypoint 4 Reached - Run Script"),
+(23600,0,3,0,40,0,100,0,6,23600,0,0,0,54,180000,0,0,0,0,0,1,0,0,0,0,0,0,0,"Apprentice Garion - On Waypoint 6 Reached - Pause Waypoint"),
+(2360000,9,0,0,0,0,100,0,0,0,0,0,0,54,6000,0,0,0,0,0,1,0,0,0,0,0,0,0,"Apprentice Garion - On Script - Pause Waypoint"),
+(2360000,9,1,0,0,0,100,0,1000,1000,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,"Apprentice Garion - On Script - Say Line 0"),
+(2360000,9,2,0,0,0,100,0,5000,5000,0,0,0,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,"Apprentice Garion - On Script - Say Line 1"),
+(2360001,9,0,0,0,0,100,0,0,0,0,0,0,54,14000,0,0,0,0,0,1,0,0,0,0,0,0,0,"Apprentice Garion - On Script - Pause Waypoint"),
+(2360001,9,1,0,0,0,100,0,2000,2000,0,0,0,5,16,0,0,0,0,0,1,0,0,0,0,0,0,0,"Apprentice Garion - On Script - Play Emote 'Kneel'"),
+(2360001,9,2,0,0,0,100,0,6000,6000,0,0,0,1,2,0,0,0,0,0,1,0,0,0,0,0,0,0,"Apprentice Garion - On Script - Say Line 2"),
+(2360001,9,3,0,0,0,100,0,6000,6000,0,0,0,1,0,0,0,0,0,0,19,23601,0,0,0,0,0,0,"Apprentice Garion - On Script - Say Line 0 (Apprentice Garion)");
+
+DELETE FROM `waypoints` WHERE `entry`=23600;
+INSERT INTO `waypoints` (`entry`, `pointid`, `position_x`, `position_y`, `position_z`, `point_comment`) VALUES
+(23600,1,-4044.27,-3393.28,38.1284,""),
+(23600,2,-4045.51,-3394.92,38.1749,""),
+(23600,3,-4042.77,-3396.91,38.1887,""),
+(23600,4,-4040.72,-3397.23,38.1476,""),
+(23600,5,-4043.21,-3395.28,38.1581,""),
+(23600,6,-4043.43,-3395.4,38.2663,"");
+
+UPDATE `creature` SET `guid`=125117,`movement_type`=0 WHERE `guid`=125117;
+UPDATE `creature_addon` SET `guid`=125117,`path_id`=0 WHERE `guid`=125117;
+DELETE FROM `waypoint_data` WHERE `id`=125117;
+DELETE FROM `waypoint_scripts` WHERE `id` IN (775,776,777);
+
+-- Delete non-exists db_script_string
+DELETE FROM `db_script_string` WHERE `entry` NOT IN (SELECT `dataint` FROM `waypoint_scripts`);
