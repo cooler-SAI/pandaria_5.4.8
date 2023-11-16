c5efa547f0c69c31091705288f522e465586fc3c
Terragor <81566364+zTerragor@users.noreply.github.com>
2023-11-16 13:25:19 +0500

    Update 2023_11_16_world_npc_toy_mini_maba_bomb.sql
    +

--- sql/updates/master/world/2023_11_16_world_npc_toy_mini_maba_bomb.sql
+++ sql/updates/master/world/2023_11_16_world_npc_toy_mini_maba_bomb.sql
@@ -1,5 +1,5 @@
 DELETE FROM creature_template_addon WHERE entry = 65903;
-INSERT INTO creature_template_addon VALUE
+INSERT INTO creature_template_addon VALUES
 (65903, 0, 0, 0, 0, 0, "105729");
 
 UPDATE creature_template SET ScriptName = "npc_toy_mini_maba_bomb" WHERE entry = 65903;
