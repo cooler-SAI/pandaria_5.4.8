e544eef9c62d022414bfaadef354b7a18c09baef
Terragor <81566364+zTerragor@users.noreply.github.com>
2023-11-20 07:00:40 +0300

    [DB/Pandria]: Fixed phase after quest reward. (#166)
    Database: Fixed phase after quest reward.

---
+++ sql/2023_11_16_world_03.sql
@@ -0,0 +1,5 @@
+
+DELETE FROM `spell_area` WHERE `spell`=102395;
+INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `racemask`, `gender`, `autocast`, `quest_start_status`, `quest_end_status`) VALUES
+(102395, 5846, 0, 29419, 0, 0, 2, 1, 64, 66),
+(102395, 5825, 0, 29419, 0, 0, 2, 1, 64, 66);
