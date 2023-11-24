99a7c71eb0c8a9f73b33f304138bcfdb498edafc
Terragor <81566364+zTerragor@users.noreply.github.com>
2023-11-24 11:05:23 +0300

    Core: Fix Console Error on Pandaren Race (24) has wrong teamid (42)
    Core: Fix Console Error on Pandaren Race (24) has wrong teamid (42)

--- src/server/game/Miscellaneous/SharedDefines.h
+++ src/server/game/Miscellaneous/SharedDefines.h
@@ -883,15 +883,15 @@ enum TeamId
 
 enum Team
 {
-    HORDE               = 67,
-    ALLIANCE            = 469,
-    PANDAREN_NEUTRAL    = 1249,                              // Pandaren is neutral
-    //TEAM_STEAMWHEEDLE_CARTEL = 169,                       // not used in code
-    //TEAM_ALLIANCE_FORCES     = 891,
-    //TEAM_HORDE_FORCES        = 892,
-    //TEAM_SANCTUARY           = 936,
-    //TEAM_OUTLAND             = 980,
-    TEAM_OTHER               = 0                            // if ReputationListId > 0 && Flags != FACTION_FLAG_TEAM_HEADER
+    HORDE						= 67,
+    ALLIANCE					= 469,
+    //TEAM_STEAMWHEEDLE_CARTEL  = 169,   // not used in code
+    //TEAM_ALLIANCE_FORCES      = 891,
+    //TEAM_HORDE_FORCES         = 892,
+    //TEAM_SANCTUARY            = 936,
+    //TEAM_OUTLAND              = 980,
+	PANDAREN_NEUTRAL		    = 1249,  // Pandaren is neutral on start
+    TEAM_OTHER                  = 0      // if ReputationListId > 0 && Flags != FACTION_FLAG_TEAM_HEADER
 };
 
 enum SpellEffects
