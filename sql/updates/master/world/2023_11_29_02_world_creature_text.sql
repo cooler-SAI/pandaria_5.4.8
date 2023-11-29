82434ba4cad7472dd701c818c423dd83291da7bf
leelf00 <leelf_00@126.com>
2023-11-29 18:32:17 +0800

    [Script/DK] Update missing boardcasttextid in dk script

--- sql/updates/master/world/2023_11_29_02_world_creature_text.sql
+++ sql/updates/master/world/2023_11_29_02_world_creature_text.sql
@@ -1,5 +1,5 @@
 -- Creature_text for npc_eye_of_acherus
 DELETE FROM `creature_text` WHERE `CreatureID`=28511;
-INSERT INTO `creature_text` (`CreatureID`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`SoundType`,`BroadcastTextId`, `TextRange`,`comment`) VALUES
-(28511,1,0, 'The eye of Acherus moves toward it\s target.',14,0,100,0,0,0,0,0,0, 'Q12641 - EMOTE_EYE_OF_ACHERUS_START'),
-(28511,2,0, 'The eye of Acherus subject of your control.',14,0,100,0,0,0,0,0,0, 'Q12641 - EMOTE_EYE_OF_ACHERUS_END');
+INSERT INTO `creature_text` (`CreatureID`, `GroupID`, `ID`, `Text`, `Type`, `Language`, `Probability`, `Emote`, `Duration`, `Sound`, `SoundType`, `BroadcastTextId`, `TextRange`, `comment`) 
+VALUES (28511, 0, 0, 'The Eye of Acherus launches towards its destination.', 42, 0, 100, 0, 0, 0, 0, 28507, 0, 'Eye of Acherus'),
+(28511, 1, 0, 'The Eye of Acherus is in your control.', 42, 0, 100, 0, 0, 0, 0, 28465, 0, 'Eye of Acherus');
