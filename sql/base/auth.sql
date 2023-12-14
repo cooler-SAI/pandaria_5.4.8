41b018302d4f6172b8fabbe1890ef49749264af6
Sargero <83485584+SargeroDeV@users.noreply.github.com>
2023-12-13 22:50:30 -0500

    [Conf/DB] Added missing components for proper installation (#198)
    * Conf/DB: Added missing components for proper installation
    
    * [SQL/Base] Add missing table struct for `updates` and `updates_include`

--- sql/base/auth.sql
+++ sql/base/auth.sql
@@ -1090,4 +1090,38 @@ CREATE TABLE `wow_token`  (
 -- Records of wow_token
 -- ----------------------------
 
+-- ----------------------------
+-- Table structure for updates
+-- ----------------------------
+DROP TABLE IF EXISTS `updates`;
+CREATE TABLE IF NOT EXISTS `updates` (
+  `name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'filename with extension of the update.',
+  `hash` char(40) COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT 'sha1 hash of the sql file.',
+  `state` enum('RELEASED','ARCHIVED') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'RELEASED' COMMENT 'defines if an update is released or archived.',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'timestamp when the query was applied.',
+  `speed` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'time the query takes to apply in ms.',
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='List of all applied updates in this database.';
+
+-- ----------------------------
+-- Records of updates
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for updates_include
+-- ----------------------------
+DROP TABLE IF EXISTS `updates_include`;
+CREATE TABLE IF NOT EXISTS `updates_include` (
+  `path` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'directory to include. $ means relative to the source directory.',
+  `state` enum('RELEASED','ARCHIVED') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'RELEASED' COMMENT 'defines if the directory contains released or archived updates.',
+  PRIMARY KEY (`path`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='List of directories where we want to include sql updates.';
+
+-- ----------------------------
+-- Records of updates_include
+-- ----------------------------
+INSERT INTO `updates_include` (`path`, `state`) VALUES
+('$/sql/updates/master/auth', 'RELEASED'),
+('$/sql/updates/master/auth/old', 'ARCHIVED');
+
 SET FOREIGN_KEY_CHECKS = 1;
